17f7db8b49808fde0e7fd05c5fe33c3db47e1c7f
unknown <zhangchuansong@softide.cn>
2021-03-30 10:54:50 +0800

    ??????????????

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+import Vue from "vue";
+/* eslint-disable */
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-createApp(App).use(te)
-createApp(App).use(ee)
-createApp(App).use(le)
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
